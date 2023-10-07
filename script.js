9ea4e75a3bfc31a81c97ce2aa5f0e3e804759eff
SANGHEON LEE <mongsil1204@naver.com>
2023-10-07 23:49:18 +0900

    11111

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPopUp() {
+    var pop = document.getElementsByClassName("popup")[0]
+    pop.style.display = 'flex';
+}
+
+function closePopUp() {
+    var pop = document.getElementsByClassName('popup')[0]
+    pop.style.display = 'none';
+}
